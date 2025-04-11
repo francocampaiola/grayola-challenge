2d99c82e0d16bb6ebff9f64918bec01e9e31c4c3
Franco Campaiola <francocampaiola@hotmail.com>
2025-04-11 16:35:05 -0300

    Actualiza la ruta del archivo CSS global y añade el paquete @radix-ui/react-label. Se elimina el archivo globals.css y se ajusta el layout para utilizar el nuevo archivo de estilos. Se mejora el componente de inicio de sesión con nuevos elementos de interfaz y diseño.

--- src/app/login/components/Login.tsx
+++ src/app/login/components/Login.tsx
@@ -1,5 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+
 const Login = () => {
-  return <div>Login Component</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full bg-[#FFF8EE]">
+      <div className="bg-[#FFFFFF] px-14 py-16 rounded-lg shadow-xl">
+        <Image
+          src={"/logo.png"}
+          width={200}
+          height={100}
+          alt="Logo"
+          className="mx-auto"
+        />
+        <div className="text-center mt-8">
+          <h1 className="text-4xl font-bold">Log In</h1>
+          <h1 className="text-md mt-2">Continue to your account</h1>
+        </div>
+        <div className="mt-4">
+          <Label htmlFor="email" className="mb-2">
+            Email
+          </Label>
+          <Input type="email" id="email" />
+        </div>
+        <div className="mt-4">
+          <Label htmlFor="password" className="mb-2">
+            Password
+          </Label>
+          <Input type="password" id="email" />
+        </div>
+        <Button className="default w-full mt-4 bg-[#90FE3D] text-black font-bold cursor-pointer">
+          Sign In <ArrowRight />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/styles/themes/theme.ts
@@ -0,0 +1,9 @@
+import colors from "./default/colors";
+import fontSizes from "./default/fontSizes";
+
+const theme = {
+  colors,
+  fontSizes,
+};
+
+export default theme;
