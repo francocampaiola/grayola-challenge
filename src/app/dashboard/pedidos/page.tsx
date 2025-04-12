c3daa2bbf70d8e0f700d3118d8213685492d6edc
Franco Campaiola <francocampaiola@hotmail.com>
2025-04-12 11:29:54 -0300

    Actualiza el componente de pedidos en el dashboard, añadiendo nuevos elementos visuales como avatares y etiquetas. Se mejora la estructura de la tabla con un nuevo diseño y se ajustan los encabezados y filas para una mejor presentación de los datos.

--- src/app/dashboard/pedidos/page.tsx
+++ src/app/dashboard/pedidos/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+import React from "react";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import {
   TableHeader,
   TableRow,
@@ -8,15 +11,15 @@ import {
   TableFooter,
   Table,
 } from "@/components/ui/table";
-import React from "react";
+import { Dot } from "lucide-react";
 
 const Pedidos = () => {
   return (
-    <div className="p-4">
-      <Table>
+    <div className="p-4 ">
+      <Table className="bg-gray-100 overflow-hidden">
         <TableHeader>
           <TableRow className="bg-gray-100 uppercase text-xs hover:bg-gray-100">
-            <TableHead className="w-[300px]">Titulo</TableHead>
+            <TableHead className="w-[400px]">Titulo</TableHead>
             <TableHead>Numero</TableHead>
             <TableHead>Estado</TableHead>
             <TableHead>Asignado a</TableHead>
@@ -26,14 +29,52 @@ const Pedidos = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow key={1}>
-            <TableCell className="font-medium">1</TableCell>
-            <TableCell>2</TableCell>
-            <TableCell>3</TableCell>
-            <TableCell className="text-right">4</TableCell>
+          <TableRow className="bg-white hover:bg-white">
+            <TableCell>
+              <div className="flex flex-col">
+                <div className="flex items-center gap-0 -ml-2">
+                  <Dot style={{ transform: "scale(1)", marginRight: "-4px" }} />
+                  <p className="font-bold">Carrousel de piezas gráficas</p>
+                </div>
+                <p className="text-xs text-gray-400">
+                  Diseño para redes sociales
+                </p>
+              </div>
+            </TableCell>
+            <TableCell className="font-medium">#1</TableCell>
+            <TableCell>
+              <Badge className="bg-[#F9DFE2] text-[#502e2e] font-bold">
+                Anulado
+              </Badge>
+            </TableCell>
+            <TableCell>
+              <Avatar className="w-8 h-8">
+                <AvatarFallback
+                  style={{
+                    fontSize: "12px",
+                  }}
+                >
+                  CN
+                </AvatarFallback>
+              </Avatar>
+            </TableCell>
+            <TableCell>
+              <Badge className="bg-[#DFFAE6] text-[#257643] font-bold">
+                <Dot style={{ transform: "scale(2)" }} />
+                Baja
+              </Badge>
+            </TableCell>
+            <TableCell>12/04/2025</TableCell>
+            <TableCell>12/04/2025</TableCell>
           </TableRow>
         </TableBody>
-        <TableFooter />
+        <TableFooter className="rounded-b-lg bg-gray-100 hover:bg-gray-100">
+          <TableRow className="h-10 flex items-center  rounded-lg">
+            <TableCell>
+              <p className="text-xs">Mostrando 10 de 100</p>
+            </TableCell>
+          </TableRow>
+        </TableFooter>
       </Table>
     </div>
   );
